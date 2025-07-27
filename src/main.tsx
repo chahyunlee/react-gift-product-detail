bebd4a68dd1e80056ba7ded7f8be1cf1e84fe2fc
chahyunlee <chahyun023@gmail.com>
2025-07-28 01:20:57 +0900

    chore: react-query 설치 및 QueryClientProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,21 @@ import "pretendard/dist/web/static/pretendard.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/AuthContext";
 import theme from "@/styles/theme/index";
 import App from "@/App";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
