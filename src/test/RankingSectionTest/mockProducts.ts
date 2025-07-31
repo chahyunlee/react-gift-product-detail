1381da60097d7ed79fbce64051bada80b3982709
chahyunlee <chahyun023@gmail.com>
2025-07-31 22:09:20 +0900

    feat: Ranking section 테스트 코드 작성

---
+++ src/test/RankingSectionTest/mockProducts.ts
@@ -0,0 +1,135 @@
+export const mockProducts = [
+  {
+    id: 11712379,
+    name: "부드러운 고구마 라떼 케이크",
+    price: {
+      basicPrice: 31000,
+      sellingPrice: 26350,
+      discountRate: 15,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250218142602_030fce0196af42189694554c03a54fbb.jpg",
+    brandInfo: {
+      id: 27,
+      name: "뚜레쥬르",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20250331162129_e8de4166853848729c5abad9834405b0.jpg",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 10349024,
+    name: "5만원권",
+    price: {
+      basicPrice: 50000,
+      sellingPrice: 50000,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20200924101152_16bb00f28a984b03a2efbdb9cec990d1.jpg",
+    brandInfo: {
+      id: 4784,
+      name: "성심당",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20200923160929_0ad3cff2a7564c4a967e30670b179a91.jpg",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 11477185,
+    name: "스트로베리 요거트 생크림",
+    price: {
+      basicPrice: 29000,
+      sellingPrice: 29000,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250408081816_d201654a475e46bd8a480849de51ea30.jpg",
+    brandInfo: {
+      id: 27,
+      name: "뚜레쥬르",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20250331162129_e8de4166853848729c5abad9834405b0.jpg",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 11526708,
+    name: "떠먹는 티라미수 + 아메리카노 R 2잔",
+    price: {
+      basicPrice: 16200,
+      sellingPrice: 16200,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250324154718_23d0ce897922417e83a7ac72ecfadca8.jpg",
+    brandInfo: {
+      id: 33,
+      name: "투썸플레이스",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20240318104050_6ad6dbbadf2b4acd860e549d5c0fcc5e.png",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 11169604,
+    name: "우유가득 생크림케이크 1호",
+    price: {
+      basicPrice: 33500,
+      sellingPrice: 33500,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250421155731_3de0cb1b4e994aac8d450de0b31877bc.jpg",
+    brandInfo: {
+      id: 2,
+      name: "파리바게뜨",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20241127182106_b80d1ec588bd49d985f46db44b827aa2.jpg",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 11169673,
+    name: "우유듬뿍 생크림케이크(6호)",
+    price: {
+      basicPrice: 49900,
+      sellingPrice: 49900,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250214140120_cc117535f56949a7aceb76acf62fe5e4.jpg",
+    brandInfo: {
+      id: 2,
+      name: "파리바게뜨",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20241127182106_b80d1ec588bd49d985f46db44b827aa2.jpg",
+    },
+    targetType: "ALL",
+    rankType: "MANY_WISH",
+  },
+  {
+    id: 11526850,
+    name: "떠먹는 스트로베리 초콜릿 생크림 + 떠먹는 티라미수 + 아메리카노 R 2잔",
+    price: {
+      basicPrice: 23400,
+      sellingPrice: 23400,
+      discountRate: 0,
+    },
+    imageURL:
+      "https://st.kakaocdn.net/product/gift/product/20250324154737_ade537cda4bd4280a726395739280afe.jpg",
+    brandInfo: {
+      id: 33,
+      name: "투썸플레이스",
+      imageURL:
+        "https://st.kakaocdn.net/product/gift/gift_brand/20240318104050_6ad6dbbadf2b4acd860e549d5c0fcc5e.png",
+    },
+    targetType: "MALE",
+    rankType: "MANY_RECEIVE",
+  },
+];
